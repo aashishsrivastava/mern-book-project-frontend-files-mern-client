7ae1763885b0ccca61b8e453e8419258fbd6ce72
aashishsrivastava <thissideashiah@gmail.com>
2024-10-07 14:05:03 +0530

    retry1.3

--- src/dashboard/DashboardLayout.jsx
+++ src/dashboard/DashboardLayout.jsx
@@ -1,4 +1,4 @@
-import { Sidebar } from "flowbite-react";
+// import { Sidebar } from "flowbite-react";
 import React from "react";
 import { Outlet } from "react-router-dom";
 // import SideBar from "./Sidebar";
