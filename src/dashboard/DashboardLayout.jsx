1c842e6576405c8d2680baccdeecdbd5a998c3ea
aashishsrivastava <thissideashiah@gmail.com>
2024-10-07 13:52:47 +0530

    retry1.1

--- src/dashboard/DashboardLayout.jsx
+++ src/dashboard/DashboardLayout.jsx
@@ -1,14 +1,15 @@
+import { Sidebar } from "flowbite-react";
 import React from "react";
 import { Outlet } from "react-router-dom";
 // import SideBar from "./Sidebar";
-import Sidebar from "./Sidebar";
+// import Sidebar from "./Sidebar";
 
 
 const DashboardLayout = () => {
   return (
     <>
       <div className="flex gap-5 flex-col md:flex-row">
-        <SideBar />
+        <Sidebar />
 
         <Outlet />
       </div>
