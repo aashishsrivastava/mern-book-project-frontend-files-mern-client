1e7dece1e547005cc8c1e16eb077c2e8de469be4
aashishsrivastava <thissideashiah@gmail.com>
2024-10-07 14:15:29 +0530

    retry1.4

--- src/dashboard/DashboardLayout.jsx
+++ src/dashboard/DashboardLayout.jsx
@@ -2,13 +2,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 // import SideBar from "./Sidebar";
-import Sidebar from "./Sidebar";
+// import Sidebar from "./Sidebar";
+import SideBar from "./SideBar";
+
 
 const DashboardLayout = () => {
   return (
     <>
       <div className="flex gap-5 flex-col md:flex-row">
-        <Sidebar />
+        {/* <Sidebar /> */}
+        <SideBar/>
 
         <Outlet />
       </div>
